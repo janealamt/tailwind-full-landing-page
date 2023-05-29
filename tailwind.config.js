a1f3d89cacd9c90ff0a9c6cc3179d15e6675d9d2
janealamt <janealamtopu647@gmail.com>
2023-05-29 13:40:54 -0700

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary-colors":"#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blob": "#A427DF",
+      }
+
+    },
+    container:{
+      center:true,
+   
+    padding:{
+      default:'20px',
+      md:'50px'
+    }
+  }
   },
   plugins: [],
 }
