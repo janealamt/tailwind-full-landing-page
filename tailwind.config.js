ca65771a558fe0f638cea5699ba917e450538843
janealamt <janealamtopu647@gmail.com>
2023-06-07 10:48:26 -0700

    new tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin=require('tailwindcss/plugin')
+const rotateY=plugin(function({addUtilities}){
+  addUtilities ({
+    '.rotate-y-180':{
+      transform:"rotateY(180deg)"
+    },
+    '.-rotate-y-180':{
+      transform:"rotateY(-180deg)"
+    }
+  })
+}
+
+
+
+)
+
 module.exports = {
   content: ['*'],
   theme: {
@@ -23,6 +40,6 @@ module.exports = {
     }
   }
   },
-  plugins: [],
+  plugins: [rotateY],
 }
 
